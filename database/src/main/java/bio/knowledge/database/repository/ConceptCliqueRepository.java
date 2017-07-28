8388d896fd3c676752ac069f05e927122b6574ce
lhannest <lhannest@gmail.com>
2017-07-28 13:47:05 -0700

    Implemented safe concept clique building

--- database/src/main/java/bio/knowledge/database/repository/ConceptCliqueRepository.java
+++ database/src/main/java/bio/knowledge/database/repository/ConceptCliqueRepository.java
@@ -1,6 +1,7 @@
 package bio.knowledge.database.repository;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.GraphRepository;
@@ -9,9 +10,51 @@ import org.springframework.data.repository.query.Param;
 import bio.knowledge.model.ConceptClique;
 
 public interface ConceptCliqueRepository extends GraphRepository<ConceptClique> {
-	@Query(
-			" MATCH (c:ConceptClique) WHERE ANY (x IN {conceptIds} WHERE x IN c.conceptIds) " +
-			" RETURN c LIMIT 1 "
-	)
+	public final String getConceptCliquesQuery = 
+			"MATCH (c:ConceptClique) WHERE ANY (x IN {conceptIds} WHERE x IN c.conceptIds) " +
+			"RETURN DISTINCT c as clique, FILTER (x IN {conceptIds} WHERE x IN c.conceptIds) as matchedConceptIds";
+	public final String getSingleConceptCliqueQuery = "MATCH (c:ConceptClique) WHERE ANY (x in {conceptIds} WHERE x IN c.conceptIds) RETURN c LIMIT 1";
+	
+	/**
+	 * Returns a ConceptClique that contains {@code conceptId}
+	 * @param conceptIds
+	 * @return
+	 */
+	default ConceptClique getConceptClique(String conceptId) {
+		return getConceptClique(new String[] {conceptId});
+	}
+	
+	/**
+	 * Returns a ConceptClique that contains any of {@code conceptIds}
+	 * @param conceptIds
+	 * @return
+	 */
+	@Query(getSingleConceptCliqueQuery)
 	public ConceptClique getConceptClique(@Param("conceptIds") List<String> conceptIds);
+	
+	/**
+	 * Returns a ConceptClique that contains any of {@code conceptIds}
+	 * @param conceptIds
+	 * @return
+	 */
+	@Query(getSingleConceptCliqueQuery)
+	public ConceptClique getConceptClique(@Param("conceptIds") String[] conceptIds);
+	
+	/**
+	 * Returns a list of maps, each containing a clique and the subset of {@code conceptIds} that are
+	 * found within that clique
+	 * @param conceptIds
+	 * @return
+	 */
+	@Query(getConceptCliquesQuery)
+	public List<Map<String, Object>> getConceptCliques(@Param("conceptIds") List<String> conceptIds);
+	
+	/**
+	 * Returns a list of maps, each containing a clique and the subset of {@code conceptIds} that are
+	 * found within that clique
+	 * @param conceptIds
+	 * @return
+	 */
+	@Query(getConceptCliquesQuery)
+	public List<Map<String, Object>> getConceptCliques(@Param("conceptIds") String[] conceptIds);
 }
--- models/src/main/java/bio/knowledge/model/ConceptClique.java
+++ models/src/main/java/bio/knowledge/model/ConceptClique.java
@@ -3,14 +3,43 @@ package bio.knowledge.model;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import bio.knowledge.model.core.neo4j.Neo4jAbstractAnnotatedEntity;
 
 public class ConceptClique extends Neo4jAbstractAnnotatedEntity {
-	private Set<String> conceptIds = new HashSet<>();
+	
+	public static boolean notDisjoint(ConceptClique clique1, ConceptClique clique2) {
+		return ! Collections.disjoint(clique1.getConceptIds(), clique2.getConceptIds());
+	}
+	
+	public static Set<String> unionOfConceptIds(Collection<ConceptClique> cliques) {
+		return cliques.stream().map(
+				clique -> { return clique.getConceptIds(); }
+		).flatMap(List::stream).collect(Collectors.toSet());
+	}
+	
+	private Set<String> conceptIds = new HashSet<String>();
+	
+	public boolean isEmpty() {
+		return conceptIds.isEmpty();
+	}
+	
+	public int size() {
+		return conceptIds.size();
+	}
+	
+	public boolean addAll(Collection collection) {
+		return conceptIds.addAll(collection);
+	}
+	
+	public boolean addAll(ConceptClique clique) {
+		return conceptIds.addAll(clique.conceptIds);
+	}
 	
 	public ConceptClique() {
 		
@@ -26,5 +55,10 @@ public class ConceptClique extends Neo4jAbstractAnnotatedEntity {
 	
 	public List<String> getConceptIds() {
 		return new ArrayList<String>(conceptIds);
+	}
+	
+	@Override
+	public String toString() {
+		return this.getClass().getSimpleName();
 	}
 }
