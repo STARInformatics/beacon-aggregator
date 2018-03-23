d6c5f8f3f22a525c18e01197e40302849c9b3f21
Richard Bruskiewich <richard@starinformatics.com>
2018-03-23 13:38:42 -0700

    BLM: terms resolved in ontology (hard coded... need Biolink lib?)

--- ontology/src/main/java/bio/knowledge/ontology/mapping/BeaconBiolinkMappingIndex.java
+++ ontology/src/main/java/bio/knowledge/ontology/mapping/BeaconBiolinkMappingIndex.java
@@ -13,11 +13,12 @@ public class BeaconBiolinkMappingIndex extends HashMap<Integer, BiolinkModelMapp
 
 	private static final long serialVersionUID = -8922021938416438973L;
 	
+	private final DirectBiolinkMapping directBiolinkMapping     = new DirectBiolinkMapping();
 	private final UMLSBiolinkMapping     umlsBiolinkMapping     = new UMLSBiolinkMapping();
 	private final WikiDataBiolinkMapping wikidataBiolinkMapping = new WikiDataBiolinkMapping();
 	
 	public BeaconBiolinkMappingIndex() {
-		put(1,  umlsBiolinkMapping);
+		put(1,  directBiolinkMapping);
 		put(2,  wikidataBiolinkMapping);
 		put(5,  umlsBiolinkMapping);
 		put(11, umlsBiolinkMapping);
