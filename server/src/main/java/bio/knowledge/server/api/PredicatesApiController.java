d620f3bc39481ae4f7c4515b2a2a17fc898a6fa1
Richard Bruskiewich <richard.bruskiewich@delphinai.com>
2019-08-21 00:27:56 +0100

    Fixed compile errors of new 0.9.2 Reasoner API merge into KBA

--- server/src/main/java/bio/knowledge/server/api/PredicatesApiController.java
+++ server/src/main/java/bio/knowledge/server/api/PredicatesApiController.java
@@ -1,9 +1,15 @@
 package bio.knowledge.server.api;
 
+import bio.knowledge.server.controller.ControllerImpl;
 import bio.knowledge.server.model.ServerPredicate;
 import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import java.util.Optional;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2019-08-20T20:17:56.260Z[GMT]")
 @Controller
 public class PredicatesApiController implements PredicatesApi {
@@ -19,6 +25,9 @@ public class PredicatesApiController implements PredicatesApi {
     public PredicatesApiDelegate getDelegate() {
         return delegate;
     }
+
+    @Autowired
+    ControllerImpl ctrl;
 
     public ResponseEntity<List<ServerPredicate>> getPredicatesDetails(@ApiParam(value = "set of aggregator indices of beacons to constrain predicates returned ") @RequestParam(value = "beacons", required = false) List<Integer> beacons) {
         return ctrl.getPredicatesDetails(beacons);
--- server/src/main/java/bio/knowledge/server/api/PredicatesApiDelegate.java
+++ server/src/main/java/bio/knowledge/server/api/PredicatesApiDelegate.java
@@ -1,19 +1,17 @@
 package bio.knowledge.server.api;
 
-import java.util.Map;
+import java.util.*;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.swagger.annotations.*;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.multipart.MultipartFile;
-import java.io.IOException;
+
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 /**
  * A delegate to be called by the {@link PredicatesApiController}}.
@@ -43,14 +41,23 @@ public interface PredicatesApiDelegate {
         if(getObjectMapper().isPresent() && getAcceptHeader().isPresent()) {
             if (getAcceptHeader().get().contains("application/json")) {
                 try {
-                    return new ResponseEntity<>(getObjectMapper().get().readValue("{
-  "chemical_substance" : {
-    "gene" : [ "directly_interacts_with", "decreases_activity_of" ]
-  }
-}", Map.class), HttpStatus.NOT_IMPLEMENTED);
-                } catch (IOException e) {
+                    List<String> examplePredicates = new ArrayList<>();
+                    examplePredicates.add("directly_interacts_with");
+                    examplePredicates.add("decreases_activity_of");
+
+                    Map<String,List<String>> exampleObject = new HashMap<>();
+                    exampleObject.put("gene", examplePredicates);
+
+                    Map<String, Map<String,List<String>>> exampleSubject = new HashMap<>();
+                    exampleSubject.put("chemical_substance", exampleObject);
+
+                    return new ResponseEntity<Map<String, Map<String,List<String>>>> (
+                            exampleSubject,
+                            HttpStatus.NOT_IMPLEMENTED
+                    );
+                } catch (Exception e) {
                     log.error("Couldn't serialize response for content type application/json", e);
-                    return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+                    return new ResponseEntity<Map<String, Map<String, List<String>>>>(HttpStatus.INTERNAL_SERVER_ERROR);
                 }
             }
         } else {
